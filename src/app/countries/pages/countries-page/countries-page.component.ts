e1e4f97dfabb73c4e142bfc626f12c2ffa369cf8
Diego Dominguez <diego_dominguez@fragua.com.mx>
2025-10-06 15:33:56 -0600

    Nested selects using HTTP requests and RXJS operators

--- src/app/countries/pages/countries-page/countries-page.component.ts
+++ src/app/countries/pages/countries-page/countries-page.component.ts
@@ -1,8 +1,75 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, effect, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/countries.interface';
+import { filter, map, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-countries-page',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './countries-page.component.html',
 })
-export class CountriesPageComponent {}
+export class CountriesPageComponent {
+  private fb = inject(FormBuilder);
+  private countriesService = inject(CountriesService);
+
+  regions = signal(this.countriesService.regions);
+
+  countriesByRegion = signal<Country[]>([]);
+  borders = signal<Country[]>([]);
+
+  myForm = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  });
+
+  //! SHOULD UNSUBSCRIBE ON COMPONENT DESTROY
+  /* selectedRegionChanged = this.myForm.get('region')!.valueChanges.subscribe((value) => {
+    console.log({ value });
+    }); */
+
+  //! SHOULD UNSUBSCRIBE ON COMPONENT DESTROY
+  onFormChanged = effect((onCleanup) => {
+    const selectedRegionSubscription = this.onRegionChange();
+    const selectedCountrySubscription = this.onCountryChange();
+
+    onCleanup(() => {
+      selectedRegionSubscription.unsubscribe();
+      selectedCountrySubscription.unsubscribe();
+    });
+  });
+
+  onRegionChange() {
+    return this.myForm
+      .get('region')!
+      .valueChanges.pipe(
+        tap(() => this.myForm.get('country')!.setValue('')),
+        tap(() => this.myForm.get('border')!.setValue('')),
+        tap(() => {
+          this.countriesByRegion.set([]);
+          this.borders.set([]);
+        }),
+        map((region) => region!.toLowerCase()),
+        switchMap((region) => this.countriesService.getCountriesByRegion(region!))
+      )
+      .subscribe((countries) => {
+        this.countriesByRegion.set(countries);
+      });
+  }
+
+  onCountryChange() {
+    return this.myForm
+      .get('country')!
+      .valueChanges.pipe(
+        tap(() => this.myForm.get('border')!.setValue('')),
+        filter((value) => value!.length > 0),
+        switchMap((code) => this.countriesService.getCountryByAlphaCode(code!)),
+        switchMap(({ borders }) => this.countriesService.getCountryNamesByCode(borders))
+      )
+      .subscribe((countries) => {
+        this.borders.set(countries);
+      });
+  }
+}
